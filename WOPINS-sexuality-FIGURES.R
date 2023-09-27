85f20648aefd7f804cc58731b4a693e6fd7eb5f9
jacobtnyoung <CRJ605@asu.edu>
2023-09-27 12:35:34 -0700

    updates to figure plot

--- WOPINS-sexuality-FIGURES.R
+++ WOPINS-sexuality-FIGURES.R
@@ -1,8 +1,20 @@
+# ============================================================================ #
+# figures file: this file creates the figures.
 
+# ----
+# clear workspace and call libraries needed
 
-need the plots of the networks as well
+rm( list = ls() )
 
+library( here )      # for reading local file paths
+library( sna )       # for working with network data
+library( network )   # for working with network data
+library( ergm )      # for the erg models
+library( stargazer ) # for the tables
 
+# ----
+# load the estimated models
+load( here( "WOPINS-sexuality-ERGM-RESULTS.RData" ) )
 
 
 # ----
@@ -19,23 +31,79 @@ u3.p.model.gof
 plot( u3.p.model.gof )
 
 
-
-
-# ============================================================================ #
-# tables file: this file creates the ergms table.
-
 # ----
-# clear workspace and call libraries needed
+# figures
 
-rm( list = ls() )
+# set up the colors
+u2.col <- ifelse( u2.net %v% "sex_minority" == 1, "red", "white" )
+u3.col <- ifelse( u3.net %v% "sex_minority" == 1, "red", "white" )
 
-library( here )      # for reading local file paths
-library( sna )       # for working with network data
-library( network )   # for working with network data
-library( ergm )      # for the erg models
-library( stargazer ) # for the tables
 
-# ----
-# load the estimated models
-load( here( "pins-w1-race-ERGM-RESULTS.RData" ) )
+# networks for friendship
+op <- par( mai = c( 0.2,0.2,0.8,0.2 ), omi = c( 0.1,0.1,0.1,0.1 ) )
+set.seed( 081922 )
+gplot( u2.net,
+       edge.col = "grey40",
+       vertex.col = u2.col,
+       displayisolates = FALSE
+)
+
+op <- par( mai = c( 0.2,0.2,0.8,0.2 ), omi = c( 0.1,0.1,0.1,0.1 ) )
+set.seed( 081922 )
+gplot( u3.net,
+       edge.col = "grey40",
+       vertex.col = u3.col,
+       displayisolates = FALSE
+)
+
+
+
+# networks for power/influence
+op <- par( mai = c( 0.2,0.2,0.8,0.2 ), omi = c( 0.1,0.1,0.1,0.1 ) )
+set.seed( 081922 )
+gplot( u2.p.net,
+       edge.col = "grey40",
+       vertex.col = u2.col,
+       displayisolates = FALSE
+)
+
+op <- par( mai = c( 0.2,0.2,0.8,0.2 ), omi = c( 0.1,0.1,0.1,0.1 ) )
+set.seed( 081922 )
+gplot( u3.p.net,
+       edge.col = "grey40",
+       vertex.col = u3.col,
+       displayisolates = FALSE
+)
+
+
+# all
+op <- par( mai = c( 0.2,0.2,0.8,0.2 ), omi = c( 0.1,0.1,0.1,0.1 ), mfrow = c( 2,2 ) )
+set.seed( 081922 )
+gplot( u2.net,
+       main = "Friendship Unit 2",
+       edge.col = "grey40",
+       vertex.col = u2.col,
+       displayisolates = FALSE
+)
+
+gplot( u3.net,
+       main = "Friendship Unit 3",
+       edge.col = "grey40",
+       vertex.col = u3.col,
+       displayisolates = FALSE
+)
+
+gplot( u2.p.net,
+       main = "Power/Influence Unit 2",
+       edge.col = "grey40",
+       vertex.col = u2.col,
+       displayisolates = FALSE
+)
+
+gplot( u3.p.net,
+       main = "Power/Influence Unit 3",
+       edge.col = "grey40",
+       vertex.col = u3.col,
+       displayisolates = FALSE
+)
 
